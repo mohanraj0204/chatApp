348ab0c5e2d5d09b62e4d8fd12c5baba03a597b3
mohanraj0204 <mohan.raj@fourkites.com>
2018-12-31 11:30:37 +0530

    channel creation

--- app/assets/javascripts/channels/chatroom.coffee
+++ app/assets/javascripts/channels/chatroom.coffee
@@ -6,4 +6,21 @@ App.chatroom = App.cable.subscriptions.create "ChatroomChannel",
     # Called when the subscription has been terminated by the server
 
   received: (data) ->
+    $('#messages').append data['message']
+    $('#message_content').val ''
+    scrollToBottom()
+    return
+
+  scrollToBottom = ->
+      if $('#messages').length > 0
+        last_message = $('#messages')[0]
+        last_message.scrollTop = last_message.scrollHeight - (last_message.clientHeight)
+
+  $(document).ready ->
+    scrollToBottom()
+    return
+
+  jQuery(document).on 'turbolinks:load', ->
+    scrollToBottom()
+    return
     # Called when there's incoming data on the websocket for this channel
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,24 @@
 class MessagesController < ApplicationController
    before_action :require_user
- 
+
     def create
-     message = current_user.messages.build(message_params)
-     if message.save
+     @message = Message.new(message_params)
+     @message.user = current_user
+     if @message.save
+       ActionCable.server.broadcast 'chatroom_channel', message: render_message(@message), user: @message.user.username
+     else
        redirect_to root_path
      end
    end
- 
+
     private
- 
+
     def message_params
      params.require(:message).permit(:body)
-   end
- 
+    end
+
+    def render_message(message)
+      render(partial: 'message', locals: { message: message } )
+    end
+
   end
